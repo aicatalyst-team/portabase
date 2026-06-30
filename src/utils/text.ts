79d725cd12d5ebefcb34dd0b8d63af8e48c052cd
Charles GTE <charlesgte31@gmail.com>
2026-06-30 08:17:46 +0200

    fix: uuid v7 (#344)
    Co-authored-by: charles-gauthereau <charles.gauthereau@soluce-technologies.com>

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -10,8 +10,8 @@ export function capitalizeFirstLetter(text: string): string {
 }
 
 export function isUUID(str: string) {
-  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
-    str,
+  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-7][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
+      str,
   );
 }
 
